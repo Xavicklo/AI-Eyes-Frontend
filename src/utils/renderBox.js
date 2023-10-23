f8d54e439f8058cb71fb78563a831dda770ee614
Lanznx <109305092@g.nccu.edu.tw>
2023-10-23 21:48:30 +0800

    test frequency

--- src/utils/renderBox.js
+++ src/utils/renderBox.js
@@ -75,7 +75,7 @@ export const renderBoxes = (
     }
   }
 
-  if (counter % 10 === 0 && counter < 500) {
+  if (counter % 20 === 0 && counter < 500) {
     const text = klasses.join(" ");
     console.log(text, "text")
     if (text.length <= 0){
