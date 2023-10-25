99087533b38b092f2c8c6f476c8a3990ff0b24db
YJack0000 <jackleagends@gmail.com>
2023-10-26 01:48:39 +0800

    chore: orgnized PWA things

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      injectRegister: "script",
+      registerType: "autoUpdate",
+      // pre-cache workbox options
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+  ],
   build: {
     chunkSizeWarningLimit: 2000, // handle warning on vendor.js bundle size
   },
